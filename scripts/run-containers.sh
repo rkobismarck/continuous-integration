300b6675546f22403c8171dc654a9d3f0e1b852d
Roberto Trujillo <robertotrujillo@MacBook-Pro-de-Roberto-2.local>
2019-04-10 20:40:59 -0500

    Fixed scripts to read paths and start containers

--- scripts/run-containers.sh
+++ scripts/run-containers.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Portainer Community.
-sudo docker-compose -f ~/continuous-integration/scripts/compose-files/portainer/docker-compose.yml up
-sudo docker-compose -f ~/continuous-integration/scripts/compose-files/jenkins/docker-compose.yml up
+sudo docker-compose -f ~/continuous-integration/scripts/compose-files/portainer/docker-compose.yml up -d 
+sudo docker-compose -f ~/continuous-integration/scripts/compose-files/jenkins/docker-compose.yml up -d
